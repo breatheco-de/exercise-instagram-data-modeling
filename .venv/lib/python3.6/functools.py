62d85acdc06636aca34574d286f550bbeb69a4e9
kamimefis <camilalobos.2307@gmail.com>
2020-11-02 03:28:43 +0000

    diagrama

---
+++ .venv/lib/python3.6/functools.py
@@ -0,0 +1,1 @@
+/home/gitpod/.pyenv/versions/3.6.9/lib/python3.6/functools.py
--- src/models.py
+++ src/models.py
@@ -8,26 +8,46 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_name = Column(String(250), nullable=False)
+    first_name=Column(String(100), nullable=False)
+    last_name=Column(String(100), nullable=False )
+    email=Column(String(100), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Follower(Base):
+    __tablename__= 'follower'
+    user_from_id=Column(Integer, primary_key= True)
+    user_to_id= Column(Integer, ForeignKey('user.id'))
+    user= relationship(User)   
+  
+class Comment(Base):
+    __tablename__ = 'comment'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    comment_text = Column(String(250))
+    author_id = Column(Integer, ForeignKey('user.id'))
+    post_id= Column(Integer, ForeignKey('post.id'))
+    #user = relationship(User)
+    #post= relationship(Post)
 
     def to_dict(self):
         return {}
+
+class Post(Base):
+    __tablename__= 'post'
+    id= Column(Integer, primary_key=True)
+    user_id= Column(Integer, ForeignKey('user.id'))
+
+class Media(Base):
+    __tablename__= 'media'
+    id= Column(Integer, primary_key=True)
+    url= Column(String(100), nullable=False)       
+    post_id= Column(Integer, ForeignKey('post.id'))
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
