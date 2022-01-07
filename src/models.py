f31edcc034c5bbe5f59e9fab4daff4e11f4167d7
Criis21 <cristina_kirika@hotmail.com>
2022-01-07 19:30:43 +0000

    Final exercise

--- src/models.py
+++ src/models.py
@@ -9,40 +9,43 @@ from eralchemy import render_er
 Base = declarative_base()
 
 class User(Base):
-    __tablename__ = 'User'
-    id = Column(Integer, primary_key=True)
-    userName = Column(String(250), primary_key=True)
-    firstName = Column(String(250), nullable=False)
-    lastName = Column(String(250), nullable=False)
-    email = Column(String(250), unique=True)
-    
-  
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key = True)
+    userName = Column(String(50), nullable = False)
+    firstName = Column(String(50), nullable = False)
+    lastName = Column(String(50), nullable = False)
+    email = Column(String(50), nullable = False, unique=True)
+
+    followers = relationship('Follower')
+    comments = relationship('Comment')
+    post = relationship('Post')
+
 class Post(Base):
-    __tablename__='Post'
+    __tablename__='post'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'))
     image_url = Column(String(250))
     comments = Column(String(250))
     likes = Column(Integer)
-    user = relationship(User)
+    media = relationship('Media')
+    comments = relationship('Comment')
 
 class Comment(Base):
-    __tablename__ = 'Comment'
+    __tablename__ = 'comment'
     id = Column(Integer, primary_key=True)
-    comment_text = Column(String(250))
+    comment_text = Column(String(250), nullable = False)
     author_id = Column(Integer, ForeignKey('user.id'))
     post_id = Column(Integer, ForeignKey('post.id'))
-    post = relationship(Post)
 
 
 class Media(Base):
-    __tablename__='Media'
+    __tablename__='media'
     id = Column(Integer, primary_key=True)
-    url = Column(String(250))
+    url = Column(String(250), nullable = False)
     post_id = Column(Integer, ForeignKey('post.id'))
 
 class Follower(Base):
-    __tablename__='Follower'
+    __tablename__='follower'
     id = Column(Integer, primary_key=True)
     user_from_id = Column(Integer, ForeignKey('user.id'))
     user_to_id = Column(Integer, ForeignKey('user.id'))
