195db5e3ca1cdf62d1ca5a548940f2f54fb9526f
Tomas Gonzalez <56565994+tommygonzaleza@users.noreply.github.com>
2022-11-21 10:43:33 -0400

    Update models.py

--- src/models.py
+++ src/models.py
@@ -4,7 +4,7 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
-from eralchemy import render_er
+from eralchemy2 import render_er
 
 Base = declarative_base()
 
