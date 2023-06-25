7443c771d484b23da310a6726676d0f97f91331c
Kelvin Ruiz <kelvinruiz.developer@gmail.com>
2023-06-25 21:44:56 +0000

    modificacion de modelos

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
@@ -10,45 +10,63 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    username = Column(String(250), nullable=False)
-    email = Column(String(50), nullable=False)
+    username = Column(String(50), unique=True, nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    password = Column(String(100), nullable=False)
     number_phone = Column(String(20), nullable=False)
     country = Column(String(50), nullable=False)
     city = Column(String(50), nullable=False)
-    post = relationship('Post',back_populates='users', uselist=True)
-    reel = relationship('Reel',back_populates='users', uselist=True)
+    posts = relationship('Post', back_populates='user')
+    reels = relationship('Reel', back_populates='user')
+    comments = relationship('Comment', back_populates='user')
+
 
 class Post(Base):
     __tablename__ = 'posts'
     id = Column(Integer, primary_key=True)
-    img = Column(String(250), nullable=False) 
-    caption = Column(String(250), nullable=False)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    comment = relationship('Comment',back_populates='posts', uselist=True)
-    reaction = relationship('Reaction',back_populates='posts', uselist=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    caption = Column(String(500), nullable=False)
+    image_url = Column(String(200), nullable=False)
+    created_at = Column(DateTime, nullable=False)
+    user = relationship('User', back_populates='posts')
+    reactions = relationship('Reaction', back_populates='post')
+    comments = relationship('Comment', back_populates='post')
+
 
 class Reel(Base):
     __tablename__ = 'reels'
     id = Column(Integer, primary_key=True)
-    video = Column(String(250), nullable=False)
-    caption = Column(String(250), nullable=False)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    comment = relationship('Comment',back_populates='posts', uselist=True)
-    reaction = relationship('Reaction',back_populates='posts', uselist=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    video_url = Column(String(200), nullable=False)
+    created_at = Column(DateTime, nullable=False)
+    user = relationship('User', back_populates='reels')
+    reactions = relationship('Reaction', back_populates='reel')
+    comments = relationship('Comment', back_populates='reel')
+
 
 class Comment(Base):
     __tablename__ = 'comments'
     id = Column(Integer, primary_key=True)
-    text = Column(String(250), nullable=False)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    post_id = Column(Integer, ForeignKey('post.id'))
+    user_id = Column(Integer, ForeignKey('users.id'))
+    post_id = Column(Integer, ForeignKey('posts.id'))
+    reel_id = Column(Integer, ForeignKey('reels.id'))
+    content = Column(String(500), nullable=False)
+    created_at = Column(DateTime, nullable=False)
+    user = relationship('User', back_populates='comments')
+    post = relationship('Post', back_populates='comments')
+    reel = relationship('Reel', back_populates='comments')
+
 
 class Reaction(Base):
     __tablename__ = 'reactions'
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.id')) 
-    post_id = Column(Integer, ForeignKey('post.id'))   
+    user_id = Column(Integer, ForeignKey('users.id'))
+    post_id = Column(Integer, ForeignKey('posts.id'))
+    reel_id = Column(Integer, ForeignKey('reels.id'))
+    reaction_type = Column(String(20), nullable=False)
+    user = relationship('User')
+    post = relationship('Post')
+    reel = relationship('Reel')
 
 try:
     result = render_er(Base, 'diagram.png')
