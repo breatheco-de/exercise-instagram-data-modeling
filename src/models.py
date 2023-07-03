db7f36e08e5c387a76d77d240e7dffd1b3d7adaa
JSamper <60151922+jorgesamper@users.noreply.github.com>
2023-07-03 19:29:25 +0000

    diagram.png

--- src/models.py
+++ src/models.py
@@ -5,33 +5,53 @@ from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
+
+# Creamos una instancia de Base de SQLAlchemy para declarar modelos
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+# Definición del modelo User
+class User(Base):
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    posts = relationship("Post", backref="user", cascade="all, delete-orphan")
+    followers = relationship("Follow", foreign_keys="Follow.followed_id", backref="followed")
+    following = relationship("Follow", foreign_keys="Follow.follower_id", backref="follower")
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+# Definición del modelo Post
+class Post(Base):
+    __tablename__ = 'posts'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    description = Column(String(250))
+    image_url = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
 
-    def to_dict(self):
-        return {}
+# Definición del modelo Follow
+class Follow(Base):
+    __tablename__ = 'follows'
+    id = Column(Integer, primary_key=True)
+    follower_id = Column(Integer, ForeignKey('users.id'))
+    followed_id = Column(Integer, ForeignKey('users.id'))
 
-## Draw from SQLAlchemy base
+# Definición del modelo Comment
+class Comment(Base):
+    __tablename__ = 'comments'
+    id = Column(Integer, primary_key=True)
+    text = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    post_id = Column(Integer, ForeignKey('posts.id'))
+
+# Draw from SQLAlchemy base. Generación del diagrama UML usando eralchemy2 y la instancia de Base
 try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
 except Exception as e:
     print("There was a problem genering the diagram")
     raise e
+
+# Terminal for diagram.png
+# pipenv shell
+# pip install sqlalchemy
+# python3 src/models.py
