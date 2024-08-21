12108fb593fe6fe46625ca1dec7bc633ff21cadc
Joao Nuñez <104280636+joaonunez@users.noreply.github.com>
2024-08-21 00:53:31 +0000

    instagram modeling finish

--- src/models.py
+++ src/models.py
@@ -1,29 +1,11 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, Date, func
+from sqlalchemy import Column, ForeignKey, Integer, String, Date, func, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
-
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
 
 class User(Base):
     __tablename__ = 'user'
@@ -37,9 +19,8 @@ class User(Base):
 
 class Follower(Base):
     __tablename__ = 'follower'
-    id = Column(Integer, primary_key=True)
-    user_from_id = Column(Integer, ForeignKey('user.id'))
-    user_to_id = Column(Integer, ForeignKey('user.id'))
+    user_from_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    user_to_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
 
 class Post(Base):
     __tablename__ = 'post'
@@ -48,7 +29,27 @@ class Post(Base):
     date = Column(Date, default=func.now())
     title = Column(String(100))
     description = Column(String(400))
-    
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(500))
+    author_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+class Like(Base):
+    __tablename__ = 'like'
+    id = Column(Integer, primary_key=True)
+    author_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    type = Column(Enum('image', 'video',  name='media_types'))
+    url = Column(String(300))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
 
     def to_dict(self):
         return {}
